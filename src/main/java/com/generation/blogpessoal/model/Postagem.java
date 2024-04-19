7ebdf7f1ffa105c931c677874e3d94ab2a7d959d
Douglas Papani <douglas.papani@hotmail.com>
2024-04-19 11:25:33 -0300

    Correção da Postagem model com usuario

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -41,7 +41,7 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Usuario usuario;
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -81,7 +81,14 @@ public class Postagem {
 	public void setTema(Tema tema) {
 		this.tema = tema;
 	}
-	
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	
 	
 }
